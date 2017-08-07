b97f3a2f33186a0b340fd75b1f9a46d309494c26
kolyatestgithub <30792002+kolyatestgithub@users.noreply.github.com>
2017-08-07 11:53:52 +0500

    change app.js
    For test PULL REQUEST

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const http = require('http')
     , socketIO = require('socket.io')
     , manageSockets = require('./socketIO/manageSockets');
 
+
 var app = express();
 var server = http.createServer(app);
 server.listen(3000, function () {
     console.info('Server listening on port %d ', 3000);
 });
+
 var IO = socketIO(server).listen(server);
 manageSockets.initBoss(IO);
 manageSockets.initUser(IO);
